184fb53f2cc9e6fe6d739118d15f8a24c552fa2c
Aleksey <aleksey-000@mail.ru>
2026-06-19 21:50:42 +0500

    планировщик отправлен в отдельный модуль

---
+++ bot/scheduler.py
@@ -0,0 +1,64 @@
+import logging
+from datetime import datetime, timedelta
+
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+
+from api import get_orders
+from bot.bot import bot
+from service.calculate import get_orders_rating
+from service.csv_writer import write_orders
+from settings import ACCESS_TOKEN, CHAT_ID
+
+logging.basicConfig(level=logging.INFO, format='[{asctime}] #{levelname:4} {name}:{lineno} - {message}', style='{')
+logger = logging.getLogger(__name__)
+
+
+async def send_daily_message():
+    """
+    callback-функция для инициализации процесса запроса данных и отправки ежедневоного сообщения
+    :return:
+    """
+    try:
+        orders = get_orders(ACCESS_TOKEN)
+        write_orders(orders)
+
+        rating = get_orders_rating(orders)
+        date = (datetime.today() - timedelta(days=1)).strftime('%d.%m.%Y')
+
+        if not rating:
+            text = f'Заказов на {date} зафиксировано не было :('
+        else:
+            text = (f"Доброе утро! Топ 3 артикула по заказам на {date}:\n"
+                    f"1. {rating[0][1]} ({rating[0][0]})\n"
+                    f"2. {rating[1][1]} ({rating[1][0]})\n"
+                    f"3. {rating[2][1]} ({rating[2][0]})")
+
+        await bot.send_message(chat_id=CHAT_ID, text=text)
+
+    except Exception as err:
+        logger.error(exc_info=True, msg='Ошибка при отправке сообщения')
+        await bot.send_message(chat_id=CHAT_ID, text='Непредвиденная ошибка формирования ежедневной статистики :(')
+
+
+async def on_startup():
+    """
+    Инициализация ежедневого задания при запуске бота
+    :return:
+    """
+    scheduler = AsyncIOScheduler(timezone="Europe/Moscow")
+
+    # планирование на 10 утра
+    # scheduler.add_job(
+    #     send_daily_message,
+    #     trigger="cron",
+    #     hour=10,
+    # )
+
+    # тестовое планирование (раз в мин)
+    scheduler.add_job(
+        send_daily_message,
+        trigger="interval",
+        minutes=1,
+    )
+
+    scheduler.start()
